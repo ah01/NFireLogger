241891cd8f0b77315c50a9d2725ef40d22edc86a
ah01 <horcicaa@gmail.com>
2012-04-29 22:35:50 +0200

    Nuget package

--- NFireLogger/Properties/AssemblyInfo.cs
+++ NFireLogger/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NFireLogger")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Adam Horcica 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
