ba9f6330b6bb653d18e8ac705f5eee2da61dbb92
ah01 <horcicaa@gmail.com>
2012-05-03 00:38:59 +0200

    Nuget package for NFireLogger.Log4net.

--- NugetPackage/make-package.bat
+++ NugetPackage/make-package.bat
@@ -1,13 +1,25 @@
 :: Prepare nuget package
 
-mkdir temp\lib\net40\
-mkdir temp\content\
+:: --- NFireLogger ---
 
-copy ..\NFireLogger\bin\Release\NFireLogger.dll temp\lib\net40\
-copy web.config.transform temp\content\
+mkdir nfirelogger\lib\net40\
+mkdir nfirelogger\content\
 
-copy NFireLogger.nuspec temp\
+copy ..\NFireLogger\bin\Release\NFireLogger.dll nfirelogger\lib\net40\
+copy web.config.transform nfirelogger\content\
 
-nuget pack temp\NFireLogger.nuspec
+copy NFireLogger.nuspec nfirelogger\
+
+nuget pack nfirelogger\NFireLogger.nuspec
+
+:: --- NFireLogger.Log4net ---
+
+mkdir nfirelogger-log4net\lib\net40\
+
+copy ..\NFireLogger.Log4net\bin\Release\NFireLogger.Log4net.dll nfirelogger-log4net\lib\net40\
+
+copy NFireLogger.Log4net.nuspec nfirelogger-log4net\
+
+nuget pack nfirelogger-log4net\NFireLogger.Log4net.nuspec
 
 
